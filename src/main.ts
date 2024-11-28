bd9b02475e436b8536aea09b22667d1cb255fc41
Kevin Svitač <kevin@svit.ac>
2024-11-28 11:16:46 +0100

    basic extensions rendering

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import '@/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 import App from '@/App.vue'
 import router from '@/router'
@@ -12,7 +14,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
 
 app.mount('#app')
--- src/views/ExtensionsView.vue
+++ src/views/ExtensionsView.vue
@@ -1,11 +1,57 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import extensionsData from '@/fixtures/extensions.json'
+
 export default defineComponent({
   name: 'ExtensionsView',
+  data() {
+    return {
+      extensionCategories: extensionsData.data,
+      infoText: 'Loading...',
+    }
+  },
+  computed: {
+    extensionsCount() {
+      let count = 0
+
+      for (const key in this.extensionCategories) {
+        // @ts-ignore
+        count += this.extensionCategories[key].length
+      }
+
+      return count
+    },
+  },
+  beforeMount() {
+    this.extensionCategories = extensionsData.data
+    this.infoText = `There are ${this.extensionsCount} extensions available`
+  },
 })
 </script>
 
 <template>
-  <main>extensions</main>
+  <main>
+    <div>
+      <h1>Extensions</h1>
+      <p>{{ infoText }}</p>
+    </div>
+
+    <div
+      v-if="extensionCategories !== undefined"
+      v-for="(extensions, category) in extensionCategories"
+    >
+      <h2>{{ category }}:</h2>
+
+      <div v-for="extension in extensions" class="grid grid-cols-4 gap-4">
+        <div class="flex flex-row items-center">
+          <span>ICON</span>
+          <div>
+            <h3>{{ extension.title }}</h3>
+            <p>{{ extension.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
