d150eda1ea4b33c2ec0e5afcda7a7cb0df54ad38
Kevin Svitač <kevin@svit.ac>
2024-12-05 10:28:14 +0100

    `NotFoundView`

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/home/HomeView.vue'
+import CreateForumView from '@/views/home/CreateForumView.vue'
+import SignUpView from '@/views/home/SignUpView.vue'
 import ContactView from '@/views/contact/ContactView.vue'
+import ExtensionsView from '@/views/ExtensionsView.vue'
 import SettingsView from '@/views/settings/SettingsView.vue'
 import DonateView from '@/views/DonateView.vue'
-import ExtensionsView from '@/views/ExtensionsView.vue'
-import SignUpView from '@/views/home/SignUpView.vue'
-import CreateForumView from '@/views/home/CreateForumView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -46,6 +47,10 @@ const router = createRouter({
       path: '/donate',
       name: 'donate',
       component: DonateView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: NotFoundView,
     },
   ],
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NotFoundView',
+})
+</script>
+
+<template>
+  <div>
+    <h1 class="big">404</h1>
+    <p>Page not found. <RouterLink :to="{ name: 'home' }">Return to home</RouterLink>.</p>
+  </div>
+</template>
